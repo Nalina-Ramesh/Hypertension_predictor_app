53f1e277c04557c92a2b19123e5d75d673e16abf
Nalina Ramesh <1by23cs133@bmsit.in>
2025-10-31 20:07:59 +0530

    Initial commit

--- app.py
+++ app.py
@@ -25,8 +25,12 @@ def predict():
     except Exception as e:
         return f"Error: {str(e)}"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # use Render's port
+    app.run(host="0.0.0.0", port=port)
+
 
 from pyngrok import ngrok
 ngrok.connect(5000)
